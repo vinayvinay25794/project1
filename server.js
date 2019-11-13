4c6638727ea8ae7b6ae01c31039e2d57e6f6931e
vinay <hiremathvinay8@gmail.com>
2019-11-13 15:09:07 +0530

    add keloy bg

---
+++ server.js
@@ -0,0 +1,14 @@
+var express= require("express");
+var app=express();
+
+app.get("/",function(request,response){
+    response.send("welcome");
+})
+
+app.post("/",function(request,response){
+    response.send("welcome");
+})
+
+app.listen(4000,function(err,res){
+    res.send("hi");
+})
